d100be1e3e2687839efd8303fb5a17c17e4e4c34
elixirevo <elixirevocation@gmail.com>
2022-03-11 04:33:33 +0900

    add: batch function

--- contracts/batch-mint/BatchMint.sol
+++ contracts/batch-mint/BatchMint.sol
@@ -4,24 +4,30 @@ pragma solidity ^0.8.10;
 import "../interfaces/IClonesneverdie.sol";
 
 contract BatchMint {
-  IClonesNeverDie public nft;
+	IClonesNeverDie public nft;
 	address public devAddress;
 
-  modifier onlyDev() {
+	modifier onlyDev() {
 		require(msg.sender == devAddress);
 		_;
 	}
 
-  constructor(address ca) {
-    devAddress = msg.sender;
-    setNftCA(ca);
-  }
+	constructor(address ca) {
+		devAddress = msg.sender;
+		setNftCA(ca);
+	}
 
-  function setDevAddress(address _devAddress) public onlyDev {
+	function batchMint(address to, uint256 num) public onlyDev {
+		for (uint256 i = 0; i < num; i++) {
+			nft.safeMint(to);
+		}
+	}
+
+	function setDevAddress(address _devAddress) public onlyDev {
 		devAddress = _devAddress;
 	}
 
-  function setNftCA(address ca) public onlyDev {
+	function setNftCA(address ca) public onlyDev {
 		nft = IClonesNeverDie(ca);
 	}
 }
